5336d61eb18534f847876877e56b6fd592ed5cf2
Christoph Eickhoff <Christoph.Eickhoff@cs.uni-kassel.de>
2014-07-07 15:20:08 +0200

    added active

---
+++ comfit/src/de/comfit/sport/SportActiv.java
@@ -0,0 +1,10 @@
+package de.comfit.sport;
+
+public abstract class SportActiv
+{
+public abstract void start();
+public abstract void updateProgress(int percent);
+public abstract void complete();
+
+
+}
---
+++ comfit/src/de/comfit/sport/StepActiv.java
@@ -0,0 +1,27 @@
+package de.comfit.sport;
+
+public class StepActiv extends SportActiv
+{
+
+   @Override
+   public void start()
+   {
+      // TODO Auto-generated method stub
+
+   }
+
+   @Override
+   public void updateProgress(int percent)
+   {
+      // TODO Auto-generated method stub
+
+   }
+
+   @Override
+   public void complete()
+   {
+      // TODO Auto-generated method stub
+
+   }
+
+}
